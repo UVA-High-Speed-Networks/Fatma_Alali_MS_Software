bdd6fb249bab22fc068232c6e49edfeeff2eca21
FatmaUVA <FatmaUVA@users.noreply.github.com>
2017-01-13 13:23:55 -0500

    improve increase disk script

--- other_scripts/increase-disk-cloudlab.sh
+++ other_scripts/increase-disk-cloudlab.sh
@@ -6,7 +6,8 @@
 #Follow this link to create a new partition if needed: 
 #http://ubuntuserverhelp.com/using-fdisk-to-create-a-partition/
 
-you_dir_name=/users/fha6np0
+#Note that for InstaGENI the home directory name is your UVA computing ID without the 0
+your_dir_name=/users/fha6np0
 
 #show disk info:
 df -h
@@ -16,20 +17,28 @@ lsblk
 sudo mkfs.ext4 /dev/sda4
 #Temporarily mount the new partition:
 sudo mkdir /mnt/tmp
-sudo mount /dev/sda4/ /mnt/tmp
+sudo mount /dev/sda4 /mnt/tmp
 
 #copy you directory to the new location:
-sudo rsync -avx $you_dir_name /mnt/tmp
+sudo rsync -avx $your_dir_name/ /mnt/tmp
 
+#mount the new partition as HOME with
+sudo mount /dev/sda4 $your_dir_name
+
+sleep 5
+#remove the old home directory
+sudo umount $your_dir_name
+#deletes the old home
+rm -rf $your_dir_name/*
+
+#Make your HOME directory  permanent
 #grep the UUID of the sda4 partition
 uuid=`sudo blkid | grep sda4 | awk '{print $2}' | cut -c 7- | rev | cut -c 2- | rev`
-echo UUID=$uuid  $you_dir_name    ext4    defaults   0  2 >> /etc/fstab
+sudo echo UUID=$uuid  $your_dir_name    ext4    defaults   0  2 >> /etc/fstab
 
-#remove the old home directory
-sudo umount $you_dir_name
 
 #remount
 sudo mount -a
 
 #to check where your directory is mounted now:
-df -P $you_dir_name | tail -1 | cut -d' ' -f 1
+df -P $your_dir_name | tail -1 | cut -d' ' -f 1
